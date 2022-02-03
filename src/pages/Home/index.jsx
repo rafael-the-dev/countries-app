e505106fa44dfd64f35d63a54c539d49fa863121
Rafael Tivane <luislangabusiness@gmail.com>
2022-02-03 12:42:15 +0200

    creating a search form

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,19 @@
 import { LOAD_ALL_COUNTRIES } from '../../graphql/queries';
 import { useQuery } from '@apollo/client';
-import { useEffect } from 'react'
+import { useCallback, useEffect } from 'react'
 import classNames from 'classnames'
 import { useGlobalStyles } from '../../styles'
-
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import { FormControl, InputAdornment, IconButton, InputLabel, OutlinedInput, Paper } from '@mui/material'
 
 const Home = () => {
     const globalStyles = useGlobalStyles();
+
+    const handleClick = useCallback(() => {}, [])
+
+    const handleMouseDownPassword = (event) => {
+        event.preventDefault();
+    };
 
     const { data } = useQuery(LOAD_ALL_COUNTRIES);
     useEffect(() => {
@@ -15,8 +22,33 @@ const Home = () => {
 
     return (
         <main 
-            className={classNames('py-8', globalStyles.main)}>
-            Hello
+            className={classNames('py-8', globalStyles.px, globalStyles.main)}>
+            <Paper
+                component="form"
+                elevation={0}>
+                <FormControl  
+                    fullWidth 
+                    variant="outlined">
+                    <InputLabel htmlFor="outlined-adornment-password">Search for a country</InputLabel>
+                    <OutlinedInput
+                        id="outlined-adornment-password"
+                        className={classNames('border-0')}
+                        endAdornment={
+                        <InputAdornment position="end">
+                            <IconButton
+                            aria-label="toggle password visibility"
+                            onClick={handleClick}
+                            onMouseDown={handleMouseDownPassword}
+                            edge="start"
+                            >
+                                <SearchOutlinedIcon />
+                            </IconButton>
+                        </InputAdornment>
+                        }
+                        label="Search for a country"
+                    />
+                </FormControl>
+            </Paper>
         </main>
     )
 };
